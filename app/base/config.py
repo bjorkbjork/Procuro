f049a4f4f0cbc6c3d1d03681ec5ea468c31e465b
bjorkbjork <francois@francoisvankempen.com>
2026-04-30 23:24:00 +1000

    Chore: init files

--- app/base/config.py
+++ app/base/config.py
@@ -4,6 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = {"env_file": ".env", "extra": "ignore"}
+
+    GMAIL_ACCOUNT: str = "sourcing.agent@example.com"
+    GMAIL_PASSWORD: str
+
     # AWS Bedrock
     BEDROCK_REGION: str = "ap-southeast-2"
     AWS_ACCESS_KEY_ID: str
@@ -23,5 +27,21 @@ class PostgresSettings(BaseSettings):
     DB_NAME: str = "koganAgentDb"
 
 
+class GoogleSettings(BaseSettings):
+    model_config = {"env_file": ".env", "extra": "ignore"}
+
+    GOOGLE_CLIENT_ID: str
+    GOOGLE_CLIENT_SECRET: str
+    GOOGLE_PROJECT_ID: str
+    GOOGLE_AUTH_URI: str
+    GOOGLE_TOKEN_URI: str
+    GOOGLE_AUTH_PROVIDER_X509_CERT_URL: str
+    GOOGLE_REFRESH_TOKEN: str
+
+    # the ID for the sheet containing both input/output tabs
+    GOOGLE_SHEET_ID: str = "1-3f1S0ditHC70fawiS6tcw2yK7GCWacecKCGXNTl3s4"
+
+
 settings = Settings()
 pg_settings = PostgresSettings()
+google_settings = GoogleSettings()
